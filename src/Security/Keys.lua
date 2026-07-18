76a6aff3d473af52d78e5bd6bc7eed56f4cdd39f
ilyesguers <yassouguers24@gmail.com>
2026-07-18 16:07:53 +0100

    Create Keys.lua

---
+++ src/Security/Keys.lua
@@ -0,0 +1,24 @@
+local Keys = {
+    ["WILI-TEST-0001-AAAA"] = {
+        plan = "lifetime",
+        owner = "Owner",
+        active = true
+    },
+    ["WILI-TEST-0002-BBBB"] = {
+        plan = "monthly",
+        owner = "Tester1",
+        active = true
+    },
+    ["WILI-VIP1-2025-XXXX"] = {
+        plan = "weekly",
+        owner = "Tester2",
+        active = true
+    },
+    ["WILI-DEMO-FREE-0000"] = {
+        plan = "daily",
+        owner = "Free",
+        active = true
+    }
+}
+
+return Keys
