b0e329d0c6891c2303bcf8d42f887fb0d3e5af5d
ilyesguers <yassouguers24@gmail.com>
2026-07-18 14:42:26 +0100

    Create Loader.lua

---
+++ src/Loader.lua
@@ -0,0 +1,13 @@
+اكتب في الملف:
+
+-- WiliExplorer Loader
+-- This is the only file users will see
+-- Everything else loads from server
+
+-- Version
+local VERSION = "1.0.0"
+
+-- Placeholder - will be built step by step
+print("[WiliExplorer] Loading v"..VERSION.."...")
+print("[WiliExplorer] Checking key...")
+print("[WiliExplorer] Starting...")
