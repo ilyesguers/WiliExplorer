3832138d4191a2ecbcd95e3cc36a280e2698af05
ilyesguers <yassouguers24@gmail.com>
2026-07-18 15:59:58 +0100

    Update Config.lua

--- src/Config.lua
+++ src/Config.lua
@@ -1,22 +1,7 @@
-اكتب في الملف:
-
--- WiliExplorer Configuration
--- Do not share this file
-
 return {
     Name = "WiliExplorer",
     Version = "1.0.0",
-    
-    -- API (will be set later)
-    API_URL = "",
-    API_KEY = "",
-    
-    -- Language: "ar" or "en"
     DefaultLanguage = "en",
-    
-    -- Theme
     Theme = "space",
-    
-    -- Placeholder
     LOADED = false
 }
